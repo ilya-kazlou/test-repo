9332626fa60bbe00754a92e80c7c78c668a7e17a
CORP\Ilya.Kozlov <ilya.kazlou@kaltura.com>
2022-05-30 23:46:26 +0300

    1

--- models/Address.cs
+++ models/Address.cs
@@ -4,4 +4,6 @@ class Address {
     public string AddressLine2 { get; set; }
 
     public string AddressLine3 { get; set; }
+
+    public string AddressLine4 { get; set; } // test branch 3 change
 }
